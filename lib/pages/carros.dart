65c5a629cdcae62a0e7b5bf76202076699b16f96
ruthynathyelle <ruthy2015buritis@hotmail.com>
2023-12-03 16:23:05 -0300

    nova atualização

--- lib/pages/carros.dart
+++ lib/pages/carros.dart
@@ -13,38 +13,23 @@ class PageCar extends StatefulWidget {
 class _PageCarState extends State<PageCar> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8, 35, 8, 0),
-      child: Column(children: [
-        Center(
-          child: Container(
-            width: 380,
-            height: 200,
-            decoration: BoxDecoration(
-                color: Colors.grey.shade300,
-                borderRadius: BorderRadius.circular(12)),
-            child: const Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text('Marcas'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(12.0),
-                    child: Text('Ver Todas'),
-                  ),
-                ],
-              ),
-            ],
-            ),
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(8, 35, 8, 0),
+      child: Column(
+        children: [
+          Marcas(),
+          //Fim do card marcas
+          SizedBox(
+            height: 20,
           ),
-          
-        )
-      ],
+          CarrosDisponiveis(),
+          //fim do carros disponiveis
+          SizedBox(
+            height: 20,
+          ),
+          MaisAcessados(),
+        ],
       ),
-      
     );
   }
 }
